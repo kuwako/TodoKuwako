dc2af9acbfb9c18161457fb1236da9690ebfcbb2
kuwako <kuwako@leverages.jp>
2016-02-16 18:51:05 +0900

    TodoListのview側作成 & アダプターの骨子作成

---
+++ app/src/main/java/com/example/kuwako/todokuwako/TodoListAdapter.java
@@ -0,0 +1,32 @@
+package com.example.kuwako.todokuwako;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by kuwako on 2016/02/16.
+ */
+public class TodoListAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
+
