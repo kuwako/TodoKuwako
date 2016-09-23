cd0ce4134ae8ece2b86f19fb6bb7f5e4b31a239a
masaki_kuwako <m.kuwako0702@gmail.com>
2016-09-23 16:22:26 +0900

    Fragment大移動

---
+++ app/src/main/java/com/example/kuwako/todokuwako/fragment/EditDialogFragment.java
@@ -0,0 +1,255 @@
+package com.example.kuwako.todokuwako.fragment;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.example.kuwako.todokuwako.R;
+import com.example.kuwako.todokuwako.activity.MainActivity;
+
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EditDialogFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link EditDialogFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EditDialogFragment extends DialogFragment {
+    private OnFragmentInteractionListener mListener;
+    @BindView(R.id.editTask)
+    EditText editTask;
+    @BindView(R.id.editTime)
+    TextView editTime;
+    @BindView(R.id.saveBtn)
+    Button saveBtn;
+    @BindView(R.id.deleteBtn)
+    Button deleteBtn;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    private int mHour;
+    private int mMinute;
+    Calendar mCalendar = Calendar.getInstance();
+    DatePickerDialog datePickerDialog;
+    TimePickerDialog timePickerDialog;
+    String deadLine;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        super.onCreateDialog(savedInstanceState);
+        if (editTodo == null) {
+            dismiss();
+        }
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View content = inflater.inflate(R.layout.dialog_edit, null);
+        ButterKnife.bind(this, content);
+
+        editTask.setText(editTodo.getTask());
+        editTask.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                // キーボードを隠す処理
+                if (hasFocus == false) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+            }
+        });
+
+        // TODO そもそも期限を削除する機能も必要そう。
+        if (editTodo.getDeadline() != null) {
+            editTime.setText(editTodo.getDeadline());
+            String[] deadLineArr = editTodo.getDeadline().split(" ", 0);
+            String[] deadLineDateArr = deadLineArr[0].split("-", 0);
+            String[] deadLineTimeArr = null;
+            if (deadLineArr.length > 1) {
+                deadLineTimeArr = deadLineArr[1].split(":");
+            }
+
+            mYear = Integer.parseInt(deadLineDateArr[0]);
+            mMonth = Integer.parseInt(deadLineDateArr[1]);
+            mDay = Integer.parseInt(deadLineDateArr[2]);
+            if (deadLineTimeArr != null) {
+                mHour = Integer.parseInt(deadLineTimeArr[0]);
+                mMinute = Integer.parseInt(deadLineTimeArr[1]);
+            } else {
+                mHour = mCalendar.get(Calendar.HOUR_OF_DAY);
+                mMinute = mCalendar.get(Calendar.MINUTE);
+            }
+
+        } else {
+            editTime.setText("タスクの期限を設定");
+
+            mYear = mCalendar.get(Calendar.YEAR);
+            mMonth = mCalendar.get(Calendar.MONTH);
+            mDay = mCalendar.get(Calendar.DAY_OF_MONTH);
+            mHour = mCalendar.get(Calendar.HOUR_OF_DAY);
+            mMinute = mCalendar.get(Calendar.MINUTE);
+        }
+
+        datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mYear = year;
+                mMonth = monthOfYear;
+                mDay = dayOfMonth;
+
+                timePickerDialog.show();
+            }
+        }, mYear, mMonth, mDay);
+
+        timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                mHour = hourOfDay;
+                mMinute = minute;
+
+                deadLine = String.valueOf(mYear) + "-" +
+                        String.format("%02d", mMonth) + "-" +
+                        String.format("%02d", mDay) + " " +
+                        String.format("%02d", mHour) + ":" +
+                        String.format("%02d", mMinute);
+                mCalendar.set(mYear, mMonth, mDay, mHour, mMinute);
+                editTime.setText(deadLine);
+            }
+        }, mHour, mMinute, true);
+
+        builder.setView(content);
+        builder.setMessage("タスクを編集/削除")
+                .setNegativeButton("閉じる", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        return builder.create();
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        editTodo = null;
+        super.onDismiss(dialog);
+    }
+
+    @OnClick({R.id.editTask, R.id.editTime, R.id.saveBtn, R.id.deleteBtn})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.editTask:
+                break;
+            case R.id.editTime:
+                datePickerDialog.show();
+                break;
+            case R.id.saveBtn:
+                // タスク名がカラだったら無効
+                if (editTask.getText().toString().equals("") || editTask.getText() == null) {
+                    Toast.makeText(MainActivity.this, "タスク名が入力されていません。", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // saveを選ばれたときの処理
+                editTodo.setTask(editTask.getText().toString());
+                editTodo.setDeadline(deadLine);
+
+                saveTodo(editTodo);
+                // アラーム仕込む処理
+                // TODO 再度編集した場合にアラームならない？
+                setTodoAlarm(editTodo, (int) editTodo.getId(), mCalendar);
+                dismiss();
+                break;
+            case R.id.deleteBtn:
+                deleteTodo(editTodo);
+                dismiss();
+                break;
+        }
+    }
+
+    public EditDialogFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static EditDialogFragment newInstance(String param1, String param2) {
+        EditDialogFragment fragment = new EditDialogFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_dialog, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/example/kuwako/todokuwako/fragment/InputDialogFragment.java
@@ -0,0 +1,308 @@
+package com.example.kuwako.todokuwako.fragment;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.text.format.Time;
+
+import com.example.kuwako.todokuwako.R;
+import com.example.kuwako.todokuwako.activity.MainActivity;
+import com.example.kuwako.todokuwako.contract.TodoContract;
+import com.example.kuwako.todokuwako.model.Todo;
+import com.example.kuwako.todokuwako.sqlite.TodoOpenHelper;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link InputDialogFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link InputDialogFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InputDialogFragment extends DialogFragment {
+    @BindView(R.id.dialogEditText)
+    EditText dialogEditText;
+    @BindView(R.id.dialogBtn)
+    Button dialogBtn;
+    @BindView(R.id.edit_date)
+    TextView editDate;
+    @BindView(R.id.dateResetBtn)
+    ImageView dateResetBtn;
+    @BindView(R.id.edit_time)
+    TextView editTime;
+    private DatePickerDialog mDlgDatePicker;
+    private TimePickerDialog mDlgTimePicker;
+    private Calendar mCalendar;
+    private GregorianCalendar mMaxDate = new GregorianCalendar();
+    private GregorianCalendar mMinDate = new GregorianCalendar();
+    private DatePicker mDatePicker;
+    private TimePicker mTimePicker;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    private int mHour;
+    private int mMinute;
+    private boolean mSetTime;
+
+    private OnFragmentInteractionListener mListener;
+
+    public InputDialogFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment InputDialogFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static InputDialogFragment newInstance(String param1, String param2) {
+        InputDialogFragment fragment = new InputDialogFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_input_dialog, container, false);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View content = inflater.inflate(R.layout.dialog_input, null);
+        ButterKnife.bind(this, content);
+
+        dialogEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                // キーボードを隠す処理
+                if (hasFocus == false) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+            }
+        });
+
+        // datepicker用の初期情報取得
+        mCalendar = Calendar.getInstance();
+        mYear = mCalendar.get(Calendar.YEAR);
+        mMonth = mCalendar.get(Calendar.MONTH);
+        mDay = mCalendar.get(Calendar.DAY_OF_MONTH);
+        mHour = mCalendar.get(Calendar.HOUR_OF_DAY);
+        mMinute = mCalendar.get(Calendar.MINUTE);
+        mSetTime = false;
+
+        mDlgDatePicker = new DatePickerDialog(MainActivity.class, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                editDate.setText(String.valueOf(year) + "年" + String.format("%1$02d", monthOfYear + 1) + "月" + String.format("%1$02d", dayOfMonth) + "日");
+                mCalendar.set(year, monthOfYear, dayOfMonth);
+
+                mDlgTimePicker.show();
+            }
+        }, mYear, mMonth, mDay);
+
+        mDlgTimePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                editTime.setText(String.format("%1$02d", hourOfDay) + "時" + String.format("%1$02d", minute) + "分");
+                mCalendar.set(mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH), hourOfDay, minute);
+                mSetTime = true;
+            }
+        }, mHour, mMinute, true);
+
+        // TODO もう少しおしゃれに
+        mMaxDate.set(2020, 11, 31);
+        mMinDate.set(2016, 0, 1);
+
+        builder.setView(content);
+        builder.setMessage("タスクを登録")
+                .setNegativeButton("閉じる", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        return builder.create();
+    }
+
+    @OnClick({R.id.dialogEditText, R.id.dialogBtn, R.id.edit_date, R.id.dateResetBtn, R.id.edit_time})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.dialogEditText:
+                break;
+            case R.id.dialogBtn:
+                Todo todo = new Todo();
+                // テキストを取得
+                String sEditText = String.valueOf(dialogEditText.getText());
+
+                // テキストが空なら終わり
+                if (sEditText.equals("")) {
+                    Toast.makeText(getActivity(), "タスク名を登録してください。", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                todo.setTask(sEditText);
+
+                // 過去の日付が登録されていたらダメ
+                Calendar nowCalendar = Calendar.getInstance();
+
+                int diff = nowCalendar.compareTo(mCalendar);
+
+                if (diff > 0 && mSetTime) {
+                    Toast.makeText(getActivity(), "過去の日付は登録できません", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // 入力エリアを初期化
+                dialogEditText.setText("");
+
+                // 日付が指定されていた場合、登録
+                if (mSetTime) {
+                    // 日付を取得
+                    String deadline = String.valueOf(mCalendar.get(Calendar.YEAR)) + "-" +
+                            String.format("%02d", mCalendar.get(Calendar.MONTH) + 1) + "-" +
+                            String.format("%02d", mCalendar.get(Calendar.DAY_OF_MONTH)) + " " +
+                            String.format("%02d", mCalendar.get(Calendar.HOUR_OF_DAY)) + ":" +
+                            String.format("%02d", mCalendar.get(Calendar.MINUTE));
+                    todo.setDeadline(deadline);
+                }
+
+                // DBに追加
+                Time time = new Time("Asia/Tokyo");
+                time.setToNow();
+
+                TodoOpenHelper todoOpenHelper = new TodoOpenHelper(MainActivity.this);
+                SQLiteDatabase db = todoOpenHelper.getWritableDatabase();
+
+                ContentValues newTask = new ContentValues();
+                newTask.put(TodoContract.Todos.COL_TASK, todo.getTask());
+                newTask.put(TodoContract.Todos.COL_DEADLINE, todo.getDeadline());
+                newTask.put(TodoContract.Todos.COL_IS_DONE, 0);
+                newTask.put(TodoContract.Todos.COL_CREATED_AT, time.year + "-" + (time.month + 1) + "-" + time.monthDay);
+
+                long newId = db.insert(TodoContract.Todos.TABLE_NAME, null, newTask);
+                todo.setId(newId);
+                mList.add(0, todo);
+                mAdapter.notifyDataSetChanged();
+
+                db.close();
+
+                if (mSetTime) {
+                    // アラームの登録
+                    setTodoAlarm(todo, (int) newId, mCalendar);
+                    mSetTime = false;
+                }
+
+                dismiss();
+                break;
+            case R.id.edit_date:
+                mDatePicker = mDlgDatePicker.getDatePicker();
+                if (mDatePicker != null) {
+                    mDatePicker.setMaxDate(mMaxDate.getTimeInMillis());
+                    mDatePicker.setMinDate(mMinDate.getTimeInMillis());
+                }
+
+                // datePickerを表示
+                mDlgDatePicker.show();
+                break;
+            case R.id.dateResetBtn:
+                mCalendar = Calendar.getInstance();
+                editDate.setText("日付登録にはこちらをクリック");
+                editTime.setText("");
+
+                Log.e("@@@@@x", "x button");
+                mSetTime = false;
+                break;
+            case R.id.edit_time:
+                mDlgTimePicker.show();
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
