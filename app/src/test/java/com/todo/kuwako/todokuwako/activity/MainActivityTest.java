3470aa80d760aaedcefc40cf737e566b05d2e9fb
kuwako <kuwako@leverages.jp>
2017-03-13 18:57:37 +0900

    簡易テスト追加

---
+++ app/src/test/java/com/todo/kuwako/todokuwako/activity/MainActivityTest.java
@@ -0,0 +1,43 @@
+package com.todo.kuwako.todokuwako.activity;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by kuwako on 2017/03/13.
+ */
+public class MainActivityTest {
+    @Test
+    public void addList() throws Exception {
+        String actual = "Hello" + " " + "World.";
+
+        assertThat(actual, is("Hello World."));
+    }
+
+    @Test
+    public void setTodo() throws Exception {
+
+    }
+
+    @Test
+    public void saveTodo() throws Exception {
+
+    }
+
+    @Test
+    public void deleteTodo() throws Exception {
+
+    }
+
+    @Test
+    public void setTodoAlarm() throws Exception {
+
+    }
+
+    @Test
+    public void deleteTodoAlarm() throws Exception {
+
+    }
+
+}
