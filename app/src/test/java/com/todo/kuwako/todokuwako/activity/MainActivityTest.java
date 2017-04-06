ee9e8d65dda1bd18156210166ebc76a5b32aa9aa
kuwako <kuwako@leverages.jp>
2017-04-06 19:06:49 +0900

    やはり最新ソースがstoreに反映されていないので、調査

--- app/src/test/java/com/todo/kuwako/todokuwako/activity/MainActivityTest.java
+++ app/src/test/java/com/todo/kuwako/todokuwako/activity/MainActivityTest.java
@@ -5,8 +5,8 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.todo.kuwako.todokuwako.model.Todo;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+//import android.support.test.rule.ActivityTestRule;
+//import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Rule;
@@ -19,21 +19,21 @@ import static org.junit.Assert.*;
 /**
  * Created by kuwako on 2017/03/13.
  */
-@RunWith(AndroidJUnit4.class)
+//@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class, false, false);
-
-    @Test
-    public void addList() throws Exception {
-        MainActivity activity = activityTestRule.launchActivity(null);
-        activity.editTask.setText("");
-        activity.onCreate(new Bundle());
-        int mListNum = activity.getmList().size();
-        activity.addList();
-
-        assertThat(0, is(mListNum));
-    }
+//    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class, false, false);
+//
+//    @Test
+//    public void addList() throws Exception {
+//        MainActivity activity = activityTestRule.launchActivity(null);
+//        activity.editTask.setText("");
+//        activity.onCreate(new Bundle());
+//        int mListNum = activity.getmList().size();
+//        activity.addList();
+//
+//        assertThat(0, is(mListNum));
+//    }
 
     @Test
     public void setTodo() throws Exception {
